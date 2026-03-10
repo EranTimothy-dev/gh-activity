151ef7a794635c41172dfb95e2a1fcb8e520e179
EranTimothy-dev <eranperera.dev@gmail.com>
2026-03-10 13:34:04 +0530

    added test implementation to check the CLI11 dependecy is working

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,23 @@
 #include <iostream>
 #include <CLI/CLI.hpp>
 
-int main(int argc, char** argv) {}
+int main(int argc, char** argv) {
+
+    CLI::App app{"A simple CLI application"};
+
+    std::string username;
+    int count = 1;
+
+    // positional argument
+    app.add_option("-u, --username", username, "Github username");
+
+    // optional argument
+    app.add_option("-c,--count", count, "Number of times");
+
+    CLI11_PARSE(app, argc, argv);
+
+    std::cout << "Usermame: " << username << std::endl;
+    std::cout << "Count: " << count << std::endl;
+
+    return 0;
+}
