5797006f0eb7024737bf5d3b1b8be4d4b6716925
EranTimothy-dev <eranperera.dev@gmail.com>
2026-03-12 02:07:12 +0530

    refactored code to user proper variables and correct if else statements for conditional parsing based on cmd flags

--- src/cli/cli.cpp
+++ src/cli/cli.cpp
@@ -2,60 +2,68 @@
 #include <CLI/CLI.hpp>
 #include <iostream>
 #include <string>
+#include "../utils/formatter.hpp"
 #include "../github/github_api.hpp"
 #include "../parser/event_parser.hpp"
+#include "../parser/repo_parser.hpp"
 
 int CLIApp::run(int argc, char** argv) {
 
     CLI::App app{"GitHub Activity CLI"};
 
     std::string username;
-    // bool getRepos = false;
-    // bool getOrgs = false;
+    json j;
 
-    auto userCmd = app.add_option("-u, --username", username, "Get info for username");
+    // app.add_option("-u, --username", username, "Get info for username");
+    auto userCmd = app.add_subcommand("user", "Get info for a user");
+    userCmd->add_option("-u,--username", username, "Username")->required();
 
     auto eventCmd = app.add_subcommand("events", "Get user events");
-    eventCmd->add_option("-u, --username", username)->required();
+    std::string eventsUser;
+    eventCmd->add_option("-u, --username", eventsUser, "Username")->required();
 
     auto tuiCmd = app.add_subcommand("tui", "Launch TUI");
-    tuiCmd->add_option("-u, --username", username)->required();
+    std::string tuiUser;
+    tuiCmd->add_option("-u, --username", tuiUser, "Username")->required();
 
-    // app.add_flag("-r, --repos", getRepos, "Get repositories for username");
-    // app.add_flag("-o, --orgs", getOrgs, "Get organizations for username");
     auto reposCmd = app.add_subcommand("repos", "Get repositories for user");
-    reposCmd->add_option("-u, --username", username)->required();
+    std::string reposUser;
+    reposCmd->add_option("-u, --username", reposUser, "Username")->required();
 
     CLI11_PARSE(app, argc, argv);
 
-    if (*userCmd) {
+    if (app.got_subcommand(userCmd)) {
         std::cout << "Fetching total repositories for user: " << username << std::endl;
         std::string data = github::get_user_info(username);
         std::string formatted = format_response(data);
         std::cout << "Total Repositories: " << formatted << std::endl;
-    }
-
-    if (*eventCmd) {
+    } else if (app.got_subcommand(eventCmd)) {
         std::cout << "Fetching activity for user: " << username << std::endl;
-        std::string data = github::get_user_events(username);
+        std::string data = github::get_user_events(eventsUser);
         std::vector<Event> events = parse_events(data);
         for (const auto& event : events) {
             fprintf(stdout, "Event Type: %s, Repo: %s\n", event.type.c_str(), event.repo.c_str());
         }
-        std::cout << "\n" << std::endl;
-        std::string formatted = format_response(data);
-        fprintf(stdout, "%s\n", data.c_str());
-    }
-
-    if (*reposCmd) {
+        // std::cout << "\n" << std::endl;
+        // std::string formatted = format_response(data);
+        // fprintf(stdout, "%s\n", data.c_str());
+    } else if (app.got_subcommand(reposCmd)) {
         std::cout << "Fetching repositories for user: " << username << std::endl;
-        std::string data = github::get_all_repos(username);
-        std::string formatted = format_response(data);
-        std::cout << formatted << std::endl;
-    }
-
-    if (*tuiCmd) {
-        std::cout << "Launching TUI for user: " << username << std::endl;
+        std::string data = github::get_all_repos(reposUser);
+        std::vector<repo::Repository> repos = parse_repos(data);
+        std::cout << "Total Repositories: " << repos.size() << std::endl;
+        for (const auto& repo : repos) {
+            // fprintf(stdout, "Repo: %s, Description: %s, Language: %s, Stars: %d, Last Pushed: %s \n", 
+            //     repo.name.c_str(), repo.description.c_str(), repo.language.c_str(), repo.stars, repo.pushed_at.c_str());
+            j = repo;
+            std::cout << j.dump(4) << std::endl;
+        }
+        // std::string formatted = format_response(data);
+        // std::cout << formatted << std::endl;
+    } else if (app.got_subcommand(tuiCmd)) {
+        std::cout << "Launching TUI for user: " << tuiUser << std::endl;
+    } else {
+        std::cout << app.help() << std::endl;
     }
 
     return 0;
