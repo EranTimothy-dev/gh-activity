f07d2d54797b4220ea3e8eecc5c1f6ab2820fc4a
EranTimothy-dev <eranperera.dev@gmail.com>
2026-03-14 01:57:23 +0530

    created methods to sanitise invalid usernames

--- src/utils/formatter.hpp
+++ src/utils/formatter.hpp
@@ -1,4 +1,7 @@
 #pragma once
 #include <string>
+#include <string_view>
 
 std::string format_response(const std::string& json_str);
+static bool sanitize_username(const std::string_view username);
+static bool is_valid_username(const std::string_view username);
